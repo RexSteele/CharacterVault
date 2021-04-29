ed9919893575b15c49a7d4521fd76a28b86e9cd9
instinctual07 <instinctual07@hotmail.com>
2021-04-29 12:52:41 -0600

    Edited cvAppDuck and store js to reflect sheet usage

--- characterdapp/src/redux/store.js
+++ characterdapp/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import reducer from "./modules/czAppDuck";
+import reducer from "./modules/cvAppDuck";
 
 const loggerMiddleware = createLogger(); // initialize logger
 
