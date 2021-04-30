07b5189dcc6a8369340b06720142512c75551835
cole <>
2021-04-30 17:33:30 -0600

    Changed those css names

--- characterdapp/src/components/sheetChar.js
+++ characterdapp/src/components/sheetChar.js
@@ -3,86 +3,86 @@ import "./sheetChar.css";
 
 class SheetChar extends Component {
 
-    getName () {
-       return (this.props.name).toLowerCase();
+    catMode () {
+       return (this.props.sheetClass).toLowerCase();
     }
 
     render() {
 
-        if (this.getName() === "barbarian") {
+        if (this.catMode() === "barbarian") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/barbarian.png" alt="class" />
                 </div>
             )
           }
-        if (this.getName() === "bard") {
+        if (this.catMode() === "bard") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/bard.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "cleric") {
+        if (this.catMode() === "cleric") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/cleric.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "druid") {
+        if (this.catMode() === "druid") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/druid.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "fighter") {
+        if (this.catMode() === "fighter") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/fighter.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "monk") {
+        if (this.catMode() === "monk") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/monk.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "paladin") {
+        if (this.catMode() === "paladin") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/paladin.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "ranger") {
+        if (this.catMode() === "ranger") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/ranger.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "rogue") {
+        if (this.catMode() === "rogue") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/rogue.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "rogue") {
+        if (this.catMode() === "rogue") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/rogue.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "sorcerer") {
+        if (this.catMode() === "sorcerer") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/sorcerer.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "wizard") {
+        if (this.catMode() === "wizard") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/wizard.png" alt="class" />
                 </div>
             )}
-        if (this.getName() === "warlock") {
+        if (this.catMode() === "warlock") {
             return (
                 <div className="character-class-preview">
                     <img  className="torso" src="static/icons/warlock.png" alt="class" />
