808e8b0f016c03d8b5be45428de0a159cb285327
RexSteele <rexralphsteele@gmail.com>
2021-04-29 18:01:27 -0600

    change levelup.js, fix sheetCardContent

--- characterdapp/src/components/sheetCardContent.js
+++ characterdapp/src/components/sheetCardContent.js
@@ -17,21 +17,27 @@ class SheetCardContent extends Component {
       <Card.Content>
         <div>
           {" "}
-          <SheetChar Character Name={this.props.sheet.charName} />{" "}
+          <SheetChar Character Name={this.props.sheetName} />{" "}
         </div>
         <Card.Header>
-          Level: <b>{this.props.sheet.level}</b> <br /> Name :{" "}
-          <b>{this.truncate(this.props.sheet.charName, 8, 8)}</b>
+          Level: <b>{this.props.sheet.sheetLevel}</b> <br /> Race :{" "}
+          <b>{this.truncate(this.props.sheet.sheetRace, 8, 8)}</b>
         </Card.Header>
         <Card.Description>
-          Race: {this.props.sheet.race} <br />
-          Class: {this.props.sheet.class} <br />
-          Str: {this.props.sheet.str} <br />
-          Dex: {this.props.sheet.dex} <br />
-          Con: {this.props.sheet.con} <br />
-          Int: {this.props.sheet.int} <br />
-          Wis: {this.props.sheet.wis} <br />
-          Cha: {this.props.sheet.cha} <br />
+          key={this.props.key}
+          sheetId={this.props.toString()}
+          sheetName={this.props.sheetName}
+          sheetRace={this.props.sheetRace}
+          sheetClass={this.props.sheetClass}
+          sheetLevel={this.props.sheetLevel}
+          sheetStr={this.props.sheetStr}
+          sheetDex={this.props.sheetDex}
+          sheetCon={this.props.sheetCon}
+          sheetInt={this.props.sheetInt}
+          sheetWis={this.props.sheetWis}
+          sheetCha={this.props.sheetCha}
+          sheetReadyTime={this.props.sheetReadyTime}
+          sheetOwner={this.props.sheetOwner}
         </Card.Description>
       </Card.Content>
     );
